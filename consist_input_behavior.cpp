370475ee779d392625a7aab988b9f3401c845fb7
冷罗KS <68413810+KSroido@users.noreply.github.com>
2020-10-21 14:37:27 +0800

    Update consist_input_behavior.cpp

--- consist_input_behavior.cpp
+++ consist_input_behavior.cpp
@@ -6,7 +6,7 @@ int main()
 	char b;
 	int a, c,temp , zhengshu;
 	cout << "Input numerator/denominator:";
-	cin >> a >> b >> c;
+	cin >> a >> b >> c;     //当进行连续输入时的变量不是相同类型的,比如char和int,那么就不需要空白符隔开各个待输入的字符
 	temp = a % c;
 	zhengshu = (a - temp)/c;
 	cout <<"=" <<zhengshu << "U" << temp << "/" << c;
