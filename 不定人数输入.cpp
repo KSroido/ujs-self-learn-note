7e436e6cc80e87797e0e974771c85c4e63782d4f
冷罗KS <68413810+KSroido@users.noreply.github.com>
2020-10-27 20:57:58 +0800

    Add files via upload

---
+++ 不定人数输入.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double n,nums;   //ÈËÊı
+	double temp, total;
+	cout << "Input num of student:" << endl;
+	total = 0;
+	cin >> n;
+	nums = n;
+	while (n > 0)
+	{
+		cin >> temp;
+		total = total + temp;
+		n--;
+	}
+	cout << "totalScore:" << total<<endl;
+	cout << "Average:" << total / nums;
+
+	return 0;
+
+
+
+}
